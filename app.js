e34262dd5b4a458eee2bbe321f5c0319f50cb721
Ana Marr <acarolinasim@gmail.com>
2020-08-01 14:26:00 +0930

    heroku

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(5000, ()=> {
+app.listen(process.env.PORT || 5000, ()=> {
   console.log('express server started');
 })
