f78304f0885444fffd327dc9862d13ec30f5d059
Ana Marr <acarolinasim@gmail.com>
2020-08-01 16:26:10 +0930

    Updates

---
+++ precache-manifest.f97a3e536ee68683806916e5fd05ac6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ecf29ce4ef6109b2b1cf5db2c02bc72",
+    "url": "/Map_App/index.html"
+  },
+  {
+    "revision": "9de4e54b4fd5581d52dc",
+    "url": "/Map_App/static/css/main.e6ca2187.chunk.css"
+  },
+  {
+    "revision": "08520ebfc43ee86ad9b1",
+    "url": "/Map_App/static/js/2.ae43faf9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Map_App/static/js/2.ae43faf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de4e54b4fd5581d52dc",
+    "url": "/Map_App/static/js/main.8ae48138.chunk.js"
+  },
+  {
+    "revision": "741efd377690feb6c122",
+    "url": "/Map_App/static/js/runtime-main.59f50706.js"
+  }
+]);
