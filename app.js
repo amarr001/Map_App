df383429774f41816b624a91a051d783b5e3b9a6
Ana Marr <acarolinasim@gmail.com>
2020-08-01 14:58:22 +0930

    heroku

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const path = require("path");
 app.use(cookieParser());
 app.use(express.json());
 
-
-
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static('client/build')); 
+  server.use('*', express.static('client/build')); // Added this     
+}
+/*
 if (process.env.NODE_ENV === "production") {
 app.use(express.static("client/build"));
-}
+}*/
 
 mongoose.connect('mongodb://localhost:27017/mernauth', {useNewUrlParser : true, useUnifiedTopology: true}, ()=>{
   console.log('successfully connected to database');
