6772ce8fbbe82177a23f4654374b48ffc60d88a1
Ana Marr <acarolinasim@gmail.com>
2020-08-03 10:40:57 +0930

    gitgnore

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const path = require("path");
 app.use(cookieParser());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/mernauth', {useNewUrlParser : true, useUnifiedTopology: true}, ()=>{
+  console.log('successfully connected to database');
+});
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));     
 }
-
-mongoose.connect('mongodb://localhost:27017/mernauth', {useNewUrlParser : true, useUnifiedTopology: true}, ()=>{
-  console.log('successfully connected to database');
-});
 
 const userRouter = require('./routes/User');
 app.use('/user',userRouter);
